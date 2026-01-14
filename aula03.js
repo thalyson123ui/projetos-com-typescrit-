3f3b078c7eb04391a5495a6739eb011c7f00f2ac
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-14 14:43:57 -0300

    atualização

--- aula03.js
+++ aula03.js
@@ -1,11 +1,11 @@
-var Doces = /** @class */ (function () {
-    function Doces(sabor, preço) {
+var Doce = /** @class */ (function () {
+    function Doce(sabor, preco) {
         this.sabor = sabor;
-        this.preço = preço;
+        this.preco = preco;
     }
-    return Doces;
+    return Doce;
 }());
-var brigadeiro = new Doces("chocolate", 5);
-var beijinho = new Doces("coco", 4);
-console.log("O sabor do brigadeiro \u00E9 ".concat(brigadeiro.sabor, " e o pre\u00E7o \u00E9 R$").concat(brigadeiro.preço));
-console.log("O sabor do beijinho \u00E9 ".concat(beijinho.sabor, " e o pre\u00E7o \u00E9 R$").concat(beijinho.preço));
+var brigadeiro = new Doce("chocolate", 5);
+var beijinho = new Doce("coco", 4);
+console.log("O sabor do brigadeiro \u00E9 ".concat(brigadeiro.sabor, " e o pre\u00E7o \u00E9 R$").concat(brigadeiro.preco));
+console.log("O sabor do beijinho \u00E9 ".concat(beijinho.sabor, " e o pre\u00E7o \u00E9 R$").concat(beijinho.preco));
