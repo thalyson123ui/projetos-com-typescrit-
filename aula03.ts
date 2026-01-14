7c87c33ee5759c23d216e335e3fa2eff1000555d
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-14 14:41:56 -0300

    atualização

--- aula03.ts
+++ aula03.ts
@@ -1,10 +1,9 @@
-class Doces {
-    constructor(public sabor: string, public preço: number) {
-    }
+class Doce {
+  constructor(public sabor: string, public preco: number) {}
 }
 
-const brigadeiro = new Doces("chocolate", 5);
-const beijinho = new Doces("coco", 4);
+const brigadeiro = new Doce("chocolate", 5);
+const beijinho = new Doce("coco", 4);
 
-console.log(`O sabor do brigadeiro é ${brigadeiro.sabor} e o preço é R$${brigadeiro.preço}`);
-console.log(`O sabor do beijinho é ${beijinho.sabor} e o preço é R$${beijinho.preço}`);
+console.log(`O sabor do brigadeiro é ${brigadeiro.sabor} e o preço é R$${brigadeiro.preco}`);
+console.log(`O sabor do beijinho é ${beijinho.sabor} e o preço é R$${beijinho.preco}`);
