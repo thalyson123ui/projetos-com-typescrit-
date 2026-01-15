5952581b6afbad88d6f221718d24ff431a544b0b
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-15 11:35:42 -0300

    atualização

--- aula04.ts
+++ aula04.ts
@@ -1,1 +1,7 @@
-console.log("Olá, mundo! Esta é a aula 04 de TypeScript.");
+let nome="thalyson";
+let curso="engenharia da computação";
+let inicio=2020;
+let conclusao=2023;
+let universidade="UNIPINHAL";
+
+console.log(`Olá, meu nome é ${nome}, curso ${curso} na ${universidade}, iniciei em ${inicio} e concluí em ${conclusao}.`);
