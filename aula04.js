6466ebb203a10e49adf6521a316e6912de11df64
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-15 11:37:29 -0300

    atualização

--- aula04.js
+++ aula04.js
@@ -1,1 +1,6 @@
-console.log("Olá, mundo! Esta é a aula 04 de TypeScript.");
+var nome = "thalyson";
+var curso = "engenharia da computação";
+var inicio = 2020;
+var conclusao = 2023;
+var universidade = "UNIPINHAL";
+console.log("Ol\u00E1, meu nome \u00E9 ".concat(nome, ", curso ").concat(curso, " na ").concat(universidade, ", iniciei em ").concat(inicio, " e conclu\u00ED em ").concat(conclusao, "."));
