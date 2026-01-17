f28cb34f35abe7591fb6fea67bd2ee09a4a38d5d
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-17 09:20:52 -0300

    atualização

---
+++ aula012.ts
@@ -0,0 +1,9 @@
+// funções em TypeScript
+
+// função com retorno
+function soma(a: number, b: number): number {
+    return a + b;
+}
+
+const resultado = soma(5, 10);
+console.log(`A soma é: ${resultado}`);
