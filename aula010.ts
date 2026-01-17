e4f5311283e730a6a7c89a5de2131a1d68cec30d
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-17 08:35:06 -0300

    atualização

--- aula010.ts
+++ aula010.ts
@@ -1,18 +1,9 @@
-// null - undefined
+let vnome: string|null;
+vnome =null;
+console.log(vnome);
 
-let minhaVar: null = null;
-let varIndefinida: undefined = undefined;
-let varNumero: number = 42;
+let vnome2: any;
+console.log(vnome2);
 
-// Atribuições válidas
-minhaVar = null;
-varIndefinida = undefined;
-
-// Atribuições inválidas (descomente para ver os erros)
-// minhaVar = 10; // Erro: Tipo 'number' não pode ser atribuído ao tipo 'null'.
-// varIndefinida = "texto"; // Erro: Tipo 'string' não pode ser atribuído ao tipo 'undefined'.
-// varNumero = null; // Erro: Tipo 'null' não pode ser atribuído ao tipo 'number'.
-
-console.log(minhaVar);
-console.log(varIndefinida);
-console.log(varNumero);
+let vnome3: unknown;
+console.log(vnome3);
