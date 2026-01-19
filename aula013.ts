659b68690a6803caf05203fc6b7028e78473700b
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-19 08:46:47 -0300

    atualização

--- aula013.ts
+++ aula013.ts
@@ -1,5 +1,16 @@
-function soma(n1:number=0,n2:number=0): number {
+function soma(n1: number = 0, n2: number = 0): number {
     return n1 + n2;
 }
 
-console.log(soma(5,5));
+console.log(soma(5, 5));
+
+function novoUser(user: string, pass: string, nome?: string): void {
+    console.log(`user: ${user}`);
+    console.log(`pass: ${pass}`);
+
+    if (nome) {
+        console.log(`nome: ${nome}`);
+    }
+}
+
+novoUser("thalyson", "1234");
