cf9dc254ff106afcb3e2792615a60dd6cea15166
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-20 10:56:21 -0300

    atualização

---
+++ aula016.ts
@@ -0,0 +1,14 @@
+class computador {
+    nome: string="thpc";
+    ram: number=0;
+    cpu: string="0";
+    gpu: string="0";
+}
+
+const comp1 = new computador();
+const comp2 = new computador();
+const comp3 = new computador();
+
+console.log(comp1);
+console.log(comp2);
+console.log(comp3);
