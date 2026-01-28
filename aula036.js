9bec5eae45d31a5da0b9747d27055d574befb01a
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-28 09:31:53 -0300

    atualização

---
+++ aula036.js
@@ -0,0 +1,8 @@
+// desestrututração de arrays parte 2
+var notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+// const [nota1, nota2, nota3, nota4, nota5] = notas
+// console.log(nota1, nota2, nota3, nota4, nota5)
+// const [nota1, , nota3, , nota5] = notas
+// console.log(nota1, nota3, nota5)
+var _a = notas[0], nota1 = _a === void 0 ? 0 : _a, _b = notas[1], nota2 = _b === void 0 ? 0 : _b, _c = notas[2], nota3 = _c === void 0 ? 0 : _c, _d = notas[3], nota4 = _d === void 0 ? 0 : _d, _e = notas[4], nota5 = _e === void 0 ? 0 : _e, _f = notas[5], nota6 = _f === void 0 ? 0 : _f;
+console.log(nota1, nota2, nota3, nota4, nota5, nota6);
